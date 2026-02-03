800ca56c723bb7711b0916175cf2db95c34895ac
Pavan Kumar Padavala <rokzpavan143@gmail.com>
2026-02-03 10:55:46 +0530

    Update auth.controller.js

--- src/controller/auth/auth.controller.js
+++ src/controller/auth/auth.controller.js
@@ -71,11 +71,8 @@ export const registerHandler = async (req, res) => {
 			"Verify your email",
 			`<p>Please click the link below:</p>
 			 <a href="${verifyUrl}">${verifyUrl}</a>`
-<<<<<<< HEAD
 		).catch((err) => console.error("Email error:", err.message));
-=======
-		);
->>>>>>> 09645d13982231df2bffe173c42aa0fe0528c958
+
 
 		return res.status(201).json({
 			message: "Registration successful! Please check your email to verify your account.",
